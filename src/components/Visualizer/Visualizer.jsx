2ffb80f1a4a3c6556ad22ee042685dd1b4462266
Serhiy7 <ssukalo79@gmail.com>
2025-07-14 20:43:27 +0200

    ц

--- src/components/Visualizer/Visualizer.jsx
+++ src/components/Visualizer/Visualizer.jsx
@@ -1,12 +1,44 @@
 // src/components/Visualizer/Visualizer.jsx
-import React, { forwardRef, useImperativeHandle } from "react";
-import { useVisualizer } from "../../hooks/useVisualizer";
+import React, { useRef, useEffect } from "react";
 import styles from "./Visualizer.module.scss";
 
-const Visualizer = forwardRef(({ audioRef }, ref) => {
-  const { canvasRef, init } = useVisualizer(audioRef);
+export default function Visualizer() {
+  const canvasRef = useRef(null);
 
-  useImperativeHandle(ref, () => ({ init }), [init]);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    let animationId;
+
+    const draw = () => {
+      const { width, height } = canvas;
+      ctx.clearRect(0, 0, width, height);
+
+      // фон
+      ctx.fillStyle = "#222";
+      ctx.fillRect(0, 0, width, height);
+
+      // создаём градиент для баров
+      const grad = ctx.createLinearGradient(0, 0, 0, height);
+      grad.addColorStop(0, "#4caf50");
+      grad.addColorStop(1, "#fff");
+      ctx.fillStyle = grad;
+
+      const barCount = 64;
+      const barWidth = width / barCount;
+      for (let i = 0; i < barCount; i++) {
+        const value = Math.random(); // случайная высота
+        const barHeight = value * height;
+        const x = i * barWidth;
+        ctx.fillRect(x + 1, height - barHeight, barWidth - 2, barHeight);
+      }
+
+      animationId = requestAnimationFrame(draw);
+    };
+
+    draw();
+    return () => cancelAnimationFrame(animationId);
+  }, []);
 
   return (
     <div className={styles.wrapper}>
@@ -18,6 +50,4 @@ const Visualizer = forwardRef(({ audioRef }, ref) => {
       />
     </div>
   );
-});
-
-export default Visualizer;
+}
